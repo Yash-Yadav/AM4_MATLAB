2738086465e17dde9ab3f17cb56f4af471d6008f
Yash-Yadav <32845721+Yash-Yadav@users.noreply.github.com>
2018-03-10 01:19:41 +0530

    Study Simpson 1/3rd  Method

---
+++ Exp5_Fn_Integral_Simpson1_3.m
@@ -0,0 +1,20 @@
+clc
+clear
+format compact
+syms x
+y=input('Enter the Function to be integrated: ');
+x0=input('Enter x0: ');
+xn=input('Enter xn: ');
+n=input('Enter the no. of Sub-intervals: ');
+h=(xn-x0)/n; %Difference between xn and xn+1
+OddSum=0;
+EvenSum=0;
+for i=1:+2:n-1 %Intitial Value 1, Step +2 and EndVal=n-1
+    OddSum=OddSum+subs(y,x0+i*h);
+end
+for i=2:+2:n-2 %Intitial Value 2, Step +2 and EndVal=n-2
+    EvenSum=EvenSum+subs(y,x0+i*h);
+end
+F=(subs(y,x0)+subs(y,xn)+4*OddSum+2*EvenSum);
+Result=(h/3)*F;
+disp(Result);
