993a7f68b2c4b0de40c6ff7594728c3bfaab0c49
Yash-Yadav <32845721+Yash-Yadav@users.noreply.github.com>
2018-04-01 19:33:25 +0530

    Update Exp6_Fn_Integral_Trapezoidal.m

--- Exp6_Fn_Integral_Trapezoidal.m
+++ Exp6_Fn_Integral_Trapezoidal.m
@@ -7,6 +7,7 @@ x0=input('Enter x0: ');
 xn=input('Enter xn: ');
 n=input('Enter the no. of Sub-intervals: ');
 h=(xn-x0)/n; %Difference between xn and xn+1
+disp("h= "+h);
 Sum=0;
 for i=1:+1:n-1 %Intitial Value 1, Step +1 and EndVal=n-1
     Sum=Sum+subs(y,x0+i*h);
